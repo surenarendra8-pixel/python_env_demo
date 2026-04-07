33f6618be1c98fda76365be482c0172932f85040
surenarendra8-pixel <surenarendra8@gmail.com>
2026-04-07 12:09:02 +0530

    Fixed employee search filter issue

--- django_git1/settings.py
+++ django_git1/settings.py
@@ -72,10 +72,21 @@ WSGI_APPLICATION = 'django_git1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "mydatabase",
+        "USER": "root",
+        "PASSWORD": "mypassword",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
